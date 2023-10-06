8353f277f13db5a97f5bb99981dd1cec39a7ae2c
OlafGG <baezauriel01@gmail.com>
2023-10-06 14:21:58 -0600

    Añadiendo la base para crear las tablas, michole, pasame que se debe filtrar, json de prueba

--- CreatePDF.py
+++ CreatePDF.py
@@ -1,18 +1,92 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
-
-def create_pdf(file_name):
+from functions.dataFunctions import connectionDB, retrieveAllRecords, retrieveRecords, retrieveRecordByID, insertRecord, updateRecords, updateRecordByID, deleteRecords, deleteRecordByID
+import json
+#def create_pdf(file_name):
     # Crea un objeto Canvas para el archivo PDF
-    c = canvas.Canvas(file_name, pagesize=letter)
+#    c = canvas.Canvas(file_name, pagesize=letter)
 
     # Agrega texto al PDF
-    text = "¡Hola, este es un documento PDF generado con Python y reportlab!"
-    c.drawString(100, 700, text)
+#    text = "¡Hola, este es un documento PDF generado con Python y reportlab!"
+#    c.drawString(100, 700, text)
 
     # Guarda el PDF
-    c.save()
+#    c.save()
 
-if __name__ == "__main__":
-    pdf_file = "./datashets/ejemplo.pdf"
-    create_pdf(pdf_file)
-    print(f"PDF generado: {pdf_file}")
+#if __name__ == "__main__":
+#    pdf_file = "./datashets/ejemplo.pdf"
+#    create_pdf(pdf_file)
+#    print(f"PDF generado: {pdf_file}")
+
+
+#Prueba de PDF, chupala pinche michell
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Frame
+from reportlab.lib import colors
+
+# Cargar datos desde el archivo JSON
+#with open("datos.json", "r") as json_file:
+ #   datos = json.load(json_file)
+
+
+mc = connectionDB()
+db = mc[0]
+profesorI = retrieveAllRecords(db, "Profesores")
+print(profesorI)
+with open("datos.json", "r") as json_file:
+    datos = json.load(json_file)
+
+# Crear un archivo PDF llamado "tabla_desde_json.pdf"
+doc = SimpleDocTemplate("tabla_desde_json.pdf", pagesize=letter)
+
+# Crear una lista de listas a partir de los datos JSON
+data = ["_id", "Nombre", "Area", "CURP", "Diciplina", "FechaNacimiento", "IES", "RFC"]
+
+
+
+dataFinal = []
+data2 = []
+for dato in data:
+    data2.append(dato)
+dataFinal.append(data2)
+
+for key in datos:
+    info = []
+    for dato in data: 
+        if dato in key:      
+            info.append(
+                key[dato]
+            )
+        else:
+            info.append(
+                ''
+            )
+    dataFinal.append(info)
+
+print(dataFinal)
+
+
+# Crear una tabla con los datos
+tabla = Table(dataFinal)
+
+#area_delimitada.add(tabla)
+# Aplicar estilo a la tabla
+estilo = TableStyle([
+    ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+    ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+    ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+    ('GRID', (0, 0), (-1, -1), 1, '#27a39d' ),
+    ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),  # Borde interno
+    ('BOX', (0, 0), (-1, -1), 0.25, colors.black) 
+    ])
+
+tabla.setStyle(estilo)
+
+# Crear el objeto Story y agregar la tabla al contenido
+story = []
+story.append(tabla)
+
+# Construir el PDF
+doc.build(story)
