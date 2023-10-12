4d35005366a7e60f55070bc095e309f94ed4f903
OlafGG <baezauriel01@gmail.com>
2023-10-12 15:10:47 -0600

    UPD - Filtros arreglados con validacion si hay o no, al igual que añadi una prueba estatica para el codigo y la creacion de los datos con lo que se supone que va a llegar

--- CreatePDF.py
+++ CreatePDF.py
@@ -2,24 +2,6 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from functions.dataFunctions import connectionDB, retrieveAllRecords, retrieveRecords, retrieveRecordByID, insertRecord, updateRecords, updateRecordByID, deleteRecords, deleteRecordByID
 import json
-#def create_pdf(file_name):
-    # Crea un objeto Canvas para el archivo PDF
-#    c = canvas.Canvas(file_name, pagesize=letter)
-
-    # Agrega texto al PDF
-#    text = "¡Hola, este es un documento PDF generado con Python y reportlab!"
-#    c.drawString(100, 700, text)
-
-    # Guarda el PDF
-#    c.save()
-
-#if __name__ == "__main__":
-#    pdf_file = "./datashets/ejemplo.pdf"
-#    create_pdf(pdf_file)
-#    print(f"PDF generado: {pdf_file}")
-
-
-#Prueba de PDF, chupala pinche michell
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Frame
 from reportlab.lib import colors
 
@@ -76,7 +58,36 @@ def createTable(filtros = {}):
     db = mc[0]
     
     # Recupera todos los registros de la colección "Profesores" de la base de datos
-    profesorI = retrieveAllRecords(db, "Profesores")
+    #profesorI = retrieveAllRecords(db, "Profesores") #Comentado por que no puedo usar la base de datos xd
+
+    #Prueba estatica de datos
+    profesorI = [
+    {
+        "autor": "ALEJANDRO",
+        "ano": 2019,
+        "documento": "Patente",
+        "areaConocimiento": "Ciencias de la Computación",
+        "otro" : {"dia" : "no", "fecha" : 20192, "_id" : 290256}
+    },
+    {
+        "autor": "María",
+        "ano": 2020,
+        "documento": "Tesis",
+        "areaConocimiento": "Matemáticas"
+    },
+    {
+        "autor": "Carlos",
+        "ano": 2019,
+        "documento": "Artículo",
+        "areaConocimiento": "Física"
+    },
+    {
+        "autor": "Ana",
+        "ano": 2020,
+        "documento": "Tesis",
+        "areaConocimiento": "Biología"
+    }
+]
 
     # Filtra los datos de acuerdo a los filtros recibidos
     #datosFiltrados = profesorI
@@ -92,37 +103,26 @@ def createTable(filtros = {}):
 
     #Intento de arreglar el problema del codigo
     def filter_func(profesor):
+        #print("Esto es loq ue tiene profesor", profesor)
         for filtro_key, filtro_info in filtros.items():
             if filtro_info['state']:
+                #print("FiltroInfo------------------",filtro_info)
+                #print("Buscando la mierda en la sopa", filtro_key)
                 if profesor.get(filtro_key, '') != filtro_info['data']:
+                    #print("iltro Key---------------", filtro_key, "Dta que esta buscando", filtro_info['data'])
+                    #print("datos que esta buscando en profesor", profesor.get(filtro_key, ''))
                     return False
-        return True
+            return True
 
-    # Aplicar la función de filtro a los datos
-    datosFiltrados = filter(filter_func, profesorI)
 
-    # Convertir el resultado filtrado a una lista
-    datosFiltrados = list(datosFiltrados)
-
-    #if 'Nombre' in filtros:
-    #    nombre_filtro = filtros['Nombre']
-    #    datosFiltrados = filter(lambda x: x.get("Nombre") == nombre_filtro['data'], datosFiltrados)
-
-    #if 'ano' in filtros:
-    #    edad_filtro = filtros['ano']
-    #    datosFiltrados = filter(lambda x: x.get("ano", 0) == edad_filtro['data'], datosFiltrados)
-
-    #if 'documento' in filtros:
-        #documento_filtro = filtros['documento']
-        #datosFiltrados = filter(lambda x: x.get("documento") == documento_filtro['data'], datosFiltrados)
-
-    #if 'disciplina' in filtros:
-     #   disciplina_filtro = filtros['areaConocimiento']
-    #    datosFiltrados = filter(lambda x: x.get("areaConociminto") == disciplina_filtro['data'], datosFiltrados)
-
+    x = len(filtros)
+    # Aplicar la función de filtro a los datos si hay alguno
+    if x != 0: 
+        datosFiltrados1 = filter(filter_func, profesorI)
 
     # Convertir el resultado filtrado a una lista
-    #datosFiltrados = list(datosFiltrados)
+    datosFiltrados = list(datosFiltrados1)
+
 
     # Crear un archivo PDF llamado "tabla_desde_json.pdf"
     doc = SimpleDocTemplate("tabla_desde_json.pdf", pagesize=letter)
@@ -163,89 +163,12 @@ def createTable(filtros = {}):
 
     # Construir el PDF
     doc.build(story)
+    print("DOCUMENTO CREADO")
+#Prueba del codigo con datos estaticos
+filtrosPrueba = {'autor': {'state': False, 'data': 'ALEJANDRO'}, 
+         'ano': {'state': False, 'data': '2019'}, 
+        'documento': {'state': False, 'data': 'Patente'}, 
+        'areaConocimiento': {'state': False, 'data': ''}, 
+        'otro': {'state': False, 'data': 'Otros'}}
 
-#    mc = connectionDB()
-#    db = mc[0]
-#    profesorI = retrieveAllRecords(db, "Profesores")
-#    print(profesorI)
-
-
-#    with open("datos.json", "r") as json_file:
-#        datos = json.load(json_file)
-    # Crear un archivo PDF llamado "tabla_desde_json.pdf"
-#    doc = SimpleDocTemplate("tabla_desde_json.pdf", pagesize=letter)
-
-#    datoBusquedaFiltro = {}
-#    for filtrodata in filtros: 
-#        if filtrodata['state'] == True: 
-#            datoBusquedaFiltro = {
-#                filtrodata : filtrodata['data']
-#            }
-
-#    datosFiltrados = []
-#    for x in datoBusquedaFiltro: 
-#        if x == 'Nombre': 
-#            datosFiltrados = filter(lambda x: x["Nombre"] == filtrodata["Nombre"], profesorI)
-        #if x == 'ano':
-            #datosFiltrados = filter(lambda x: x["edad"] >= filtrodata["ano"], profesorI)#
-
-# Convertir el resultado a una lista
-#    personas_mayores = list(personas_mayores)
-
-    #   db.estudiantes.find({ nombre : { $in : ['Maria','José','Juan', 10] } },{nombre: 1, _id: 0})
-
-    # Crear una lista de listas a partir de los datos JSON
-#    data = ["_id", "Nombre", "Area", "CURP", "Diciplina", "FechaNacimiento", "IES", "RFC"]
-
-
-
-#    dataFinal = []
-#    data2 = []
-#    for dato in data:
-#        data2.append(dato)
-#    dataFinal.append(data2)
-
-#    for key in datos:
-#        info = []
-#        for dato in data: 
-#            if dato in key:      
-#                info.append(
-#                    key[dato]
-#                )
-#            else:
-#                info.append(
-#                    ''
-#                )
-#        dataFinal.append(info)#
-
-#    print(dataFinal)
-
-
-    # Crear una tabla con los datos
-#    tabla = Table(dataFinal)
-
-    #area_delimitada.add(tabla)
-    # Aplicar estilo a la tabla
-#    estilo = TableStyle([
-#        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-#        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-#        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-#        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-#        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-#        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-#        ('GRID', (0, 0), (-1, -1), 1, '#27a39d' ),
-#        ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),  # Borde interno
-#        ('BOX', (0, 0), (-1, -1), 0.25, colors.black) 
-#        ])
-
-    #tabla.setStyle(estilo)
-
-    # Crear el objeto Story y agregar la tabla al contenido
-    #story = []
-    #story.append(tabla)
-
-    # Construir el PDF
-    #doc.build(story)
-
-
-
+createTable(filtrosPrueba)
