5010394313263dba55fdcccafd9ccf7c025fcb50
OlafGG <baezauriel01@gmail.com>
2023-11-17 11:44:47 -0600

    UPD - reducido el codigo, quitando partes que se repiten haciendolo más limpio

--- CreatePDF.py
+++ CreatePDF.py
@@ -14,10 +14,8 @@ def createTable(filtros = {}):
     mc = connectionDB()
     db = mc[0]
     
-    # Recupera todos los registros de la colección "Profesores" de la base de datos
-    #profesorI = retrieveAllRecords(db, "Profesores") #Comentado por que no puedo usar la base de datos xd
+    # Get para todos los datos de las tablas
 
-    #Prueba estatica de datos
     profesorI = retrieveAllRecords(db, "Profesores")
     beneficiosPROMEP = retrieveAllRecords(db, "BeneficiosPROMEP")
     cuerpoAcademico = retrieveAllRecords(db, "CuerpoAcademico")
@@ -31,17 +29,9 @@ def createTable(filtros = {}):
     tutorias = retrieveAllRecords(db, "Tutorias")
     
 
-    # Aplicar la función de filtro a los datos si hay alguno
-    
-    #datosFiltrados1 = filter(filter_func, profesorI)
-
-    # Convertir el resultado filtrado a una lista
-    datosFiltrados = profesorI
-
-    # Crear un archivo PDF llamado "tabla_desde_json.pdf"
     
 
-    # Crear una lista de nombres de columnas
+    # Titulos de las tablas
     columnasProf = ["_id", "Nombre", "FechaNacimiento", "IES"]
     columnasPROMEP = ["IES Solicitud", "Solicitud", "Vigencia", "Estado", "id Profesor"]
     columnasAcademico = ["id", "Nombre Academico", "clave", "Consolidacion"]
@@ -53,130 +43,53 @@ def createTable(filtros = {}):
     cProfesoresInvestigacion = ["_id", "Nombre", "id_profesor", "id_investigacion"]
     cProfesoresLogros = ["Nombre", "idProfesor", "idLogro"]
     cTurorias = ["Tutoria", "Nivel", "programa", "Estado"]
-    #for i in filtros:
-    #    columnas.append(i)
-
-    # Crear una lista de listas a partir de los datos filtrados
-    dataFinalProf = [columnasProf]
-    dataFinalPROMEP = [columnasPROMEP]
-    dataFinalAcademicos = [columnasAcademico]
-    dataFinalDireccion = [columnasDireccion]
-    dataFinalDocencias = [columnasDocencias]
-    dataFinalGAcademicas = [cGAcademica]
-    dataFinalInvestigacion = [cInvestigacion]
-    dataFinalLogros = [cLogros]
-    dataFinalPinvestifaciones = [cProfesoresInvestigacion]
-    dataFinalPLogros = [cProfesoresLogros]
-    dataFinalTutorias = [cTurorias]
-
-    for profesor in datosFiltrados:
-        dataFinalProf.append([
-            profesor.get("_id", ""),
-            profesor.get("Nombre", ""),
-            profesor.get("FechaNacimiento", ""),
-            profesor.get("IES", "")
-        ])
-
-    for promep in beneficiosPROMEP: 
-        dataFinalPROMEP.append([
-            promep.get("IESSolicitud", ""),
-            promep.get("Solicitud", ""),
-            promep.get("Vigencia", ""),
-            promep.get("Estado", ""),
-            promep.get("IdProfesor", "") 
-        ])
-
-    for academicos in cuerpoAcademico:
-        dataFinalAcademicos.append([
-            academicos.get("_id", ""),
-            academicos.get("NombreCuerpoAcademico"),
-            academicos.get("Clave"),
-            academicos.get("GradoConsolidacion"),
-        ]) 
-
-    for direccion in direccionIndividualizada:
-        dataFinalDireccion.append([
-            direccion.get("TituloTesisProyectoIndividual", ""),
-            direccion.get("Grado", ""),
-            direccion.get("EstadoDireccionIndividualizada", ""),
-            direccion.get("LAGCs"),
-        ])
-    for docencia in docencias:
-        dataFinalDocencias.append([
-            docencia.get("NombreCurso", ""),
-            docencia.get("InsitucionEducacionSuperior(IES)"),
-            docencia.get("DependenciaEducacionSuperior(IES)"),
-        ])
-
-    for GAcademica in gestionAcademica: 
-        dataFinalGAcademicas.append([
-            GAcademica.get("TipoGestion", ""),
-            GAcademica.get("CargoDentroComisionCuerpoColegiado", ""),
-            GAcademica.get("FuncionEncomendada", ""),
-            GAcademica.get("Aprobado", ""),
-            GAcademica.get("Estado", ""),
-        ])
-
-    for investigacion in investigaciones:
-        dataFinalInvestigacion.append([
-            investigacion.get("TituloProyecto", ""),
-            investigacion.get("NombrePatrocinador", ""),
-            investigacion.get("TipoPatrocinador")
-        ])
-
-    for logro in logros:
-        dataFinalLogros.append([
-            logro.get("Tipo", ""),
-            logro.get("Titulo", ""),
-            logro.get("Pais", ""),
-            logro.get("Ano", ""),
-        ])
     
-    for pInvestigacion in profesoresInvestigaciones:
-        dataFinalPinvestifaciones.append([
-            pInvestigacion.get("_id", ""),
-            pInvestigacion.get("Nombre", ""),
-            pInvestigacion.get("IdProfesor", ""),
-            pInvestigacion.get("IdInvestigacion", "")
-        ])
 
-    for pLogro in profesorLogros:
-        dataFinalPLogros.append([
-            pLogro.get("Nombre", ""),
-            pLogro.get("IdProfesor", ""),
-            pLogro.get("IdLogro", "")
-        ])
+    # Lista de campos que se buscaran en los datos
+    camposProfesor = ["_id", "Nombre", "FechaNacimiento", "IES"]
+    camposPROMEP = ["IESSolicitud", "Solicitud", "Vigencia", "Estado", "IdProfesor"]
+    camposCuerpoAcademico = ["_id", "NombreCuerpoAcademico", "Clave", "GradoConsolidacion"]
+    camposDirecciones = ["TituloTesisProyectoIndividual", "Grado", "EstadoDireccionIndividualizada", "LAGCs"]
+    camposDocencias = ["NombreCurso", "InsitucionEducacionSuperior(IES)", "DependenciaEducacionSuperior(IES)"]
+    camposGestionAcademica = ["TipoGestion", "CargoDentroComisionCuerpoColegiado", "FuncionEncomendada", "Aprobado", "Estado"]
+    camposInvestigacion = ["TituloProyecto", "NombrePatrocinador", "TipoPatrocinador"]
+    camposLogros = ["Tipo", "Titulo", "Pais", "Ano"]
+    camposProfesorInvestigaciones = ["_id", "Nombre", "IdProfesor", "IdInvestigacion"]
+    camposProfesoresLogros = ["Nombre", "IdProfesor", "IdLogro"]
+    camposTutorias = ["Tutoria", "Nivel", "ProgramaEducativoParticipa", "EstadoTutelaje"]
 
-    for tutoria in tutorias:
-        dataFinalTutorias.append([
-            tutoria.get("Tutoria", ""),
-            tutoria.get("Nivel", ""),
-            tutoria.get("ProgramaEducativoParticipa", ""),
-            tutoria.get("EstadoTutelaje", "")
-        ])
+    #creacion de las tablas mandando: NOMBRE DEL PDF, CAMPOS DE BUSQUEDA, DATOS DE LA BD, TITULOS DE LA TABLA
+    creacionTabla("PROFESOR",  camposProfesor, profesorI, columnasProf)
+    creacionTabla("BENEFICIOS_PROMEP",  camposPROMEP, beneficiosPROMEP, columnasPROMEP)
+    creacionTabla("CUERPO_ACADEMICO",  camposCuerpoAcademico, cuerpoAcademico, columnasAcademico)
+    creacionTabla("DIRECCIONES",  camposDirecciones, direccionIndividualizada, columnasDireccion)
+    creacionTabla("DOCENCIAS",  camposDocencias, docencias, columnasDocencias)
+    creacionTabla("GESTION_ACADEMICA",  camposGestionAcademica, gestionAcademica, cGAcademica)
+    creacionTabla("INVESTIGACION",  camposInvestigacion, investigaciones, cInvestigacion)
+    creacionTabla("LOGROS",  camposLogros, logros, cLogros)
+    creacionTabla("PROFESOR_INVESTIGACIONES",  camposProfesorInvestigaciones, profesoresInvestigaciones, cProfesoresInvestigacion)
+    creacionTabla("PROFESOR_LOGROS",  camposProfesoresLogros, profesorLogros, cProfesoresLogros)
+    creacionTabla("TUTORIAS",  camposTutorias, tutorias, cTurorias)
 
 
-
-    creacionTabla("Profesor", dataFinalProf)
-    creacionTabla("PROMEP", dataFinalPROMEP)
-    creacionTabla("Academicos", dataFinalAcademicos)
-    creacionTabla("Direccion", dataFinalDireccion)
-    creacionTabla("Docencias", dataFinalDocencias)
-    creacionTabla("Gestion Academica", dataFinalGAcademicas)
-    creacionTabla("Investigaciones", dataFinalInvestigacion)
-    creacionTabla("Logros", dataFinalLogros)
-    creacionTabla("Profesores Investigaciones", dataFinalPinvestifaciones)
-    creacionTabla("Profesores Logros", dataFinalPLogros)
-    creacionTabla("Tutorias", dataFinalTutorias)
-    
     
     print("DOCUMENTOS CREADO")
 
 
 
 
-def creacionTabla(nombreTabla, dataFinal):
+def creacionTabla(nombreTabla, camposBusqueda, dataBase, camposTitulo):
 
+    #Añadiendo titulos a la tabla
+    dataFinal = [camposTitulo]
+
+    #Repasar cada dato de la data
+    for dato in dataBase:
+        # Recorre el arreglo de los campos de busqueda para encontrarlos y añadirlos a la lista, si no los encuentra, solo deja el espacio en blanco
+        dataFinal.append([dato.get(campo, "") for campo in camposBusqueda])
+
+    
+    #Crea el documento, se coloca el nombre y el tamaño
     doc = SimpleDocTemplate(nombreTabla+".pdf", pagesize=letter)
     # Crear una tabla con los datos
     tabla = Table(dataFinal)
